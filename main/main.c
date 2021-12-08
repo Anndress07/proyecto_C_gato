9a1b0d8e479f3bd3c9a80fd703190e97232ec776
Anndress07 <92402988+Anndress07@users.noreply.github.com>
2021-12-07 21:31:27 -0600

    Update main.c

--- main/main.c
+++ main/main.c
@@ -11,16 +11,17 @@
 
 // Variables
 GtkWidget *window; // Window
-GtkLabel *mylabel; // Label
-GtkLabel *label00;
-GtkLabel *label01;
-GtkLabel *label02;
-GtkLabel *label10;
-GtkLabel *label11;
-GtkLabel *label12;
-GtkLabel *label20;
-GtkLabel *label21;
-GtkLabel *label22;
+GtkLabel *state_label;
+GtkLabel *lab1;
+GtkLabel *lab2;
+GtkLabel *lab3;
+GtkLabel *lab4;
+GtkLabel *lab5;
+GtkLabel *lab6;
+GtkLabel *lab7;
+GtkLabel *lab8;
+GtkLabel *lab9;
+GtkWidget *reset_button;
 
 void printBoard();
 void playerMove();
@@ -32,6 +33,9 @@ void checkFreeSpaces();
 char board[3][3] = {{0,0,0},
                     {0,0,0},
                     {0,0,0}};
+
+char board_reset[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
+
 const char PLAYER = 'X';
 const char COMPUTER = 'O';
 char winner = ' ';
@@ -47,20 +51,21 @@ int main(int argc, char *argv[])
 
 
 	builder = gtk_builder_new(); // Create GTK UI Builder
-	gtk_builder_add_from_file(builder, "myui2.glade", NULL); // Load our UI file
+	gtk_builder_add_from_file(builder, "game-grid.glade", NULL); // Load our UI file
 
 	// Assign the Variables
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "MyWindow")); // Load our window named MyWindow
-	mylabel = GTK_LABEL(gtk_builder_get_object(builder, "MyLabel")); // Load our label named MyLabel
-	label00 = GTK_LABEL(gtk_builder_get_object(builder, "label00"));
-	label01 = GTK_LABEL(gtk_builder_get_object(builder, "label01"));
-	label02 = GTK_LABEL(gtk_builder_get_object(builder, "label02"));
-	label10 = GTK_LABEL(gtk_builder_get_object(builder, "label10"));
-	label11 = GTK_LABEL(gtk_builder_get_object(builder, "label11"));
-	label12 = GTK_LABEL(gtk_builder_get_object(builder, "label12"));
-	label20 = GTK_LABEL(gtk_builder_get_object(builder, "label20"));
-	label21 = GTK_LABEL(gtk_builder_get_object(builder, "label21"));
-	label22 = GTK_LABEL(gtk_builder_get_object(builder, "label22"));
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "MyWindow2")); // Load our window named MyWindow
+  state_label = GTK_LABEL(gtk_builder_get_object(builder, "state"));
+  lab1 = GTK_LABEL(gtk_builder_get_object(builder, "lab1"));
+  lab2 = GTK_LABEL(gtk_builder_get_object(builder, "lab2"));
+  lab3 = GTK_LABEL(gtk_builder_get_object(builder, "lab3"));
+  lab4 = GTK_LABEL(gtk_builder_get_object(builder, "lab4"));
+  lab5 = GTK_LABEL(gtk_builder_get_object(builder, "lab5"));
+  lab6 = GTK_LABEL(gtk_builder_get_object(builder, "lab6"));
+  lab7 = GTK_LABEL(gtk_builder_get_object(builder, "lab7"));
+  lab8 = GTK_LABEL(gtk_builder_get_object(builder, "lab8"));
+  lab9 = GTK_LABEL(gtk_builder_get_object(builder, "lab9"));
+  reset_button = GTK_WIDGET(gtk_builder_get_object(builder, "reset_button"));
 	// Essential for a GTK based program
 	gtk_builder_connect_signals(builder, NULL);
 	g_object_unref(builder);
@@ -83,82 +88,81 @@ void gato_main(int x, int y){
   nuevocheckWinner();
   printBoard();
 
-} if (winnerExists == 2) {
+  } if (winnerExists == 2) {
   printf("IT'S A TIE!\n");
-}
+  gtk_label_set_text(state_label, "IT'S A TIE!");
+  }
 
-}
-  //printWinner(winner);
-
-
+  }
 // Function to exit our app
 void exit_app()
 {
+
 	printf("Exit app \n"); // Not neccesary
 	gtk_main_quit(); // Command to quit a GTK program
 }
 
 // Function when our button is pressed
-void button00_clicked()
+void button1_clicked()
 {
-	printf("Button00 clicked \n");
-	gtk_label_set_text(label00, "pressed");
+	printf("Button1 clicked \n");
+	gtk_label_set_text(lab1, "X");
   gato_main(0,0);
 }
 
-void button01_clicked()
+void button2_clicked()
 {
-	printf("Button01 clicked \n");
-	gtk_label_set_text(label01, "pressed");
+	printf("Button2 clicked \n");
+	gtk_label_set_text(lab2, "X");
   gato_main(0,1);
 }
 
-void button02_clicked()
+void button3_clicked()
 {
-	printf("Button02 clicked \n");
-	gtk_label_set_text(label02, "pressed");
+	printf("Button3 clicked \n");
+	gtk_label_set_text(lab3, "X");
   gato_main(0,2);
 }
 
-void button10_clicked()
+void button4_clicked()
 {
-	printf("Button10 clicked \n");
-	gtk_label_set_text(label10, "pressed");
+	printf("Button4 clicked \n");
+	gtk_label_set_text(lab4, "X");
   gato_main(1,0);
 }
 
-void button11_clicked()
+void button5_clicked()
 {
-	printf("Button11 clicked \n");
-	gtk_label_set_text(label11, "pressed");
+	printf("Button5 clicked \n");
+	gtk_label_set_text(lab5, "X");
   gato_main(1,1);
 }
 
-void button12_clicked()
+void button6_clicked()
 {
-	printf("Button clicked \n");
-	gtk_label_set_text(label12, "pressed");
+	printf("Button6 clicked \n");
+	gtk_label_set_text(lab6, "X");
   gato_main(1,2);
 }
 
-void button20_clicked()
+void button7_clicked()
 {
-	printf("Button20 clicked \n");
-	gtk_label_set_text(label20, "pressed");
+	printf("Button7 clicked \n");
+	gtk_label_set_text(lab7, "X");
   gato_main(2,0);
 }
 
-void button21_clicked()
+void button8_clicked()
 {
-	printf("Button21 clicked \n");
-	gtk_label_set_text(label21, "pressed");
+	printf("Button8 clicked \n");
+	gtk_label_set_text(lab8, "X");
   gato_main(2,1);
 }
 
-void button22_clicked()
+void button9_clicked()
 {
-	printf("Button22 clicked \n");
-	gtk_label_set_text(label22, "pressed");
+	printf("Button9 clicked \n");
+	gtk_label_set_text(lab9, "X");
   gato_main(2,2);
 }
 
@@ -171,6 +175,17 @@ void printBoard()
    printf(" %d | %d | %d ", board[2][0], board[2][1], board[2][2]);
    printf("\n");
 }
+
+void printReset_Board()
+{
+  printf(" %d | %d | %d ", board_reset[0][0], board_reset[0][1], board_reset[0][2]);
+  printf("\n---|---|---\n");
+  printf(" %d | %d | %d ", board_reset[1][0], board_reset[1][1], board_reset[1][2]);
+  printf("\n---|---|---\n");
+  printf(" %d | %d | %d ", board_reset[2][0], board_reset[2][1], board_reset[2][2]);
+  printf("\n");
+}
+
 void playerMove(int x, int y) {
     if (board[x][y] == 0) {
         board[x][y] = 1; // 1 es igual a la X en el gato
@@ -189,8 +204,6 @@ void computerMove() {
         board[a][b] = 2; // 2 es igual al O en el gato
         printf("computermove :board[%d][%d] = %d\n", a, b, board[a][b]);
     }
-
-
 void nuevocheckWinner() {
   // REVISANDO FILAS
   // FILA 1
@@ -281,58 +294,21 @@ if (board[0][3] == board[1][3] && board[0][3] == board[2][3] && board[0][3] != 0
    }
  }
 
-
-
-
 }
 
-// char checkWinner() {
-//    //check rows
-//    for(int i = 0; i < 3; i++)
-//    {
-//         printf("checkwinner rows :board[%d][%d] = %d\n", i, 0, board[i][0]);
-//         printf("checkwinner rows :board[%d][%d] = %d\n", i, 1, board[i][1]);
-//         printf("checkwinner rows :board[%d][%d] = %d\n", i, 2, board[i][2]);
-//         printf("-------%d ciclo------", i);
-//       if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
-//       {
-//
-//          return board[i][0];
-//       }
-//    }
-//    //check columns
-//    for(int i = 0; i < 3; i++)
-//    {
-//       if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
-//       {
-//
-//          return board[0][i];
-//       }
-//    }
-//    //check diagonals
-//    if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
-//    {
-//
-//       return board[0][0];
-//    }
-//    if(board[0][2] == board[1][1] && board[0][2] == board[2][0])
-//    {
-//
-//       return board[0][2];
-//    }
-//
-//    return ' ';
-// }
+
 
 void printWinner(int z)
 {  winnerExists = 1;
    if(z == 1) // 1 ES LA X DEL JUGADOR
    {
       printf("YOU WIN!\n");
+      gtk_label_set_text(state_label, "YOU WIN!");
    }
    else if(z == 2) // 2 ES EL CIRCULO DEL JUGADOR
    {
       printf("YOU LOSE!\n");
+      gtk_label_set_text(state_label, "YOU LOSE!");
    }
    //else if (checkFreeSpaces() == 1) {
       //printf("IT'S A TIE!");
@@ -341,7 +317,7 @@ void printWinner(int z)
 
  void checkFreeSpaces() {
    int freeSpaces = 9;
-   { //printf("in checkFreeSpaces");
+   {
 
 
       for(int i = 0; i < 3; i++)
@@ -351,13 +327,38 @@ void printWinner(int z)
             if(board[i][j] != 0)
             {
                freeSpaces--;
-
             }
          }
       }
       if (freeSpaces == 1) {
         printf("freespaces: %d\n", freeSpaces);
         winnerExists = 2;
-      }
+     }
    } printf("freespaces: %d\n", freeSpaces);
  }
+
+void reset_button_clicked() {
+  spaces = 9;
+  winnerExists = 0;
+
+  printf("RESET GRID\n");
+
+  for(int i = 0; i < 3; i++)
+  {
+     for(int j = 0; j < 3; j++)
+     { board[i][j] = 0;
+       printf("reset board[%d][%d] = %d\n", i, j, board[i][j]);
+     }
+   }
+
+  gtk_label_set_text(lab1, " ");
+  gtk_label_set_text(lab2, " ");
+  gtk_label_set_text(lab3, " ");
+  gtk_label_set_text(lab4, " ");
+  gtk_label_set_text(lab5, " ");
+  gtk_label_set_text(lab6, " ");
+  gtk_label_set_text(lab7, " ");
+  gtk_label_set_text(lab8, " ");
+  gtk_label_set_text(lab9, " ");
+  gtk_label_set_text(state_label, " ");
+}
